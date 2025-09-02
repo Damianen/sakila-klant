9c7afceeff3613a4432042a115dd250d5037337a
Damianen <113175544+Damianen@users.noreply.github.com>
2025-09-02 19:23:34 +0200

    Add pool.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,8 @@
 import express from "express";
-import mysql from "mysql2/promise";
-import fs from "fs";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const config = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-}
-
-const pool = mysql.createPool(config);
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
